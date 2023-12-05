3bb51d7b7119dbea60297e577216acb090be87bc
krobicon <113786742+krobicon@users.noreply.github.com>
2023-12-05 18:17:08 -0500

    Update Movement.cpp

--- Movement.cpp
+++ Movement.cpp
@@ -1,1 +1,16 @@
+#pragma once
+struct Movement {
+    ConfigLoader* cl;
+    XDisplay* display;
+    Level* level;
+    LocalPlayer* localPlayer;
+
+    FloatVector2D previous_weaponPunchAngles;
+
+    Movement(ConfigLoader* cl, XDisplay* display, Level* level, LocalPlayer* localPlayer) {
+        this->cl = cl;
+        this->display = display;
+        this->level = level;
+        this->localPlayer = localPlayer;
+    }
 
