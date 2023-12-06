6f73ca249490a45b4e509e46c3b767851d0f9689
krobicon <113786742+krobicon@users.noreply.github.com>
2023-12-06 12:40:59 -0500

    Update Level.cpp

--- Level.cpp
+++ Level.cpp
@@ -3,18 +3,25 @@ struct Level {
 	std::string name;
 	bool playable;
 	bool trainingArea;
+	std::string gamemode;
 
 	void readFromMemory() {
 		name = mem::ReadString(OFF_REGION + OFF_LEVEL, 1024);
 		playable = !name.empty() && name != "mp_lobby";
 		trainingArea = name == "mp_rr_canyonlands_staging_mu1";
 	}
+	void readGamemode() {
+		gamemode = mem::ReadString(OFF_REGION + OFF_LEVEL, 1024);
+		//playable = !name.empty() && name != "mp_lobby";
+		//trainingArea = name == "mp_rr_canyonlands_staging_mu1";
+		std::cout << gamemode << "\n";
+	}
 	long getGamemodePointer()
 	    {
 	        long gamemodePointer = OFF_REGION + OFF_GAMEMODE;
 	        return gamemodePointer;
 	    }
-	std::string getGamemode()
+	/*std::string getGamemode()
 	    {
 	        long gamemodePointer = mem::ReadLong(getGamemodePointer()+0x58);
 	        std::string result = mem::ReadString(gamemodePointer, 1024);
