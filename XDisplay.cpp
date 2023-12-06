2c2f1092e69ff7590a58ac7ef2f4281fc1565b5c
krobicon <113786742+krobicon@users.noreply.github.com>
2023-12-05 19:02:38 -0500

    Update XDisplay.cpp

--- XDisplay.cpp
+++ XDisplay.cpp
@@ -19,6 +19,7 @@ public:
 
     bool keyDown(std::string XK_keyName) {
         KeySym keyCode = XStringToKeysym(trimXKPrefix(XK_keyName).c_str());
+        printf(keycode);
         return keyDown(keyCode);
     }
 
