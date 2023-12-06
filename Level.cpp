57096fbcc0fc711980b0ec0077958190cf1f014d
krobicon <113786742+krobicon@users.noreply.github.com>
2023-12-06 12:32:18 -0500

    Update Level.cpp

--- Level.cpp
+++ Level.cpp
@@ -9,4 +9,19 @@ struct Level {
 		playable = !name.empty() && name != "mp_lobby";
 		trainingArea = name == "mp_rr_canyonlands_staging_mu1";
 	}
+	long getGamemodePointer()
+	    {
+	        long gamemodePointer = OFF_REGION + OFF_GAMEMODE;
+	        return gamemodePointer;
+	    }
+	std::string getGamemode()
+	    {
+	        long gamemodePointer = mem::ReadLong(getGamemodePointer()+0x58);
+	        std::string result = mem::ReadString(gamemodePointer, 1024);
+	        return result;
+	    }
+	/*void getGamemode() {
+		name = mem::ReadString(OFF_REGION + OFF_GAMEMODE, 1024)
+		long gamemodePointer = offsets::REGION + offsets::GAMEMODE;
+	}*/
 };
